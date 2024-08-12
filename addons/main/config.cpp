e157cb74fc9a9960ca6820793351ab8e86fc12b3
Gregory Sims <briggybros.2@googlemail.com>
2024-08-12 19:02:41 +0100

    Initial

---
+++ addons/main/config.cpp
@@ -0,0 +1,15 @@
+
+#include "script_component.hpp"
+
+class CfgPatches {
+	class ADDON {
+		name = CSTRING(component);
+		author = "117Briggy";
+		url = "https://github.com/Briggybros/snd";
+		requiredVersion = 2; 
+		requiredAddons[] = {};
+		units[] = {};
+		weapons[] = {};
+		VERSION_CONFIG;
+	};
+};
---
+++ addons/main/script_mod.hpp
@@ -0,0 +1,10 @@
+#define MAINPREFIX Briggybros
+#define PREFIX snd
+
+#include "script_version.hpp"
+
+#define VERSION     MAJOR.MINOR
+#define VERSION_STR MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION_AR  MAJOR,MINOR,PATCHLVL,BUILD
+
+#define REQUIRED_VERSION 2
